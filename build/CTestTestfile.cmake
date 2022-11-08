65edf055ec7ec3601b56f65f1f5ed06835e29824
Ivan Gurov <paccomaxaivan@mail.ru>
2022-11-08 11:57:39 +0700

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Project/helloworld
+# Build directory: C:/Project/helloworld/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "C:/Project/helloworld/CMakeLists.txt;74;add_test;C:/Project/helloworld/CMakeLists.txt;0;")
